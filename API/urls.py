76869ee90137a0f4c9691e2d27ccf044f8ebd7e9
Charlie Redpath <charlie.redpath@warwick.ac.uk>
2022-11-28 17:28:59 +0000

    Add comments to API code

--- API/urls.py
+++ API/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('article/<int:id>', views.get_article, name="article"),
     path('article', views.post_article, name="post_article"),
-    path("", views.redirect_docs),
+    path("", views.redirect_docs),  # Redirect empty to the documentation
     path("test", views.test)
 ]
--- API/views.py
+++ API/views.py
@@ -1,3 +1,6 @@
+"""API Endpoints"""
+
+# Imports
 import os
 import json
 import time
@@ -10,8 +13,10 @@ from typing import Dict
 from dataclasses import dataclass
 from collections import defaultdict
 
+
 @dataclass
 class User:
+  """Simple User dataclass for logging"""
   failures: int
   last_post: float
   last_get: float
@@ -20,15 +25,26 @@ USERS: Dict[str, User] = defaultdict(lambda: User(0, time.time(), time.time()))
 
 
 def test(request):
+  """Simple test endpoint"""
   return HttpResponse("Hello, world.")
 
+
 def redirect_docs(request):
+  """Redirects to root of docs"""
   return redirect("/docs")
 
+
 def get_article(request, id: int):
+  """Endpoint to get an article, with ID = id
+
+    If article id exists, returns the JSON
+  """
+
+  # Only accepts GET
   if request.method != 'GET':
     return HttpResponseBadRequest("This endpoint only accepts GET requests.")
 
+  # Try to access and load the file
   path = os.path.join(BASE_DIR, f"articles/{id}")
   try:
     with open(path) as f:
@@ -38,6 +54,7 @@ def get_article(request, id: int):
   except json.JSONDecodeError:
     return HttpResponseServerError("Bad file format, please let us know.")
 
+  # Got the file contents fine
   else:
     try:
       title = article["title"]
@@ -46,19 +63,37 @@ def get_article(request, id: int):
       date_published = article["date_published"]
     except KeyError:
       return HttpResponseServerError("Bad file format, please let us know.")
+    
+    # Got all content ok
     else:
       return HttpResponse(json.dumps({
         "id": id, "title": title, "sub_heading": sub_heading, "content": content, "date_published": date_published
       }))
 
 def post_article(request):
+  """Endpoint to add an article
+  
+    Requires POST data in form:
+      {
+        "title": <title>,
+        "sub_heading": <sub_heading>,
+        "content": <content>
+      }
+    Other fields will be ignored
+
+    On success, returns a message containing the new id
+  """
+
+  # Only accepts POST
   if request.method != 'POST':
     return HttpResponseBadRequest("This endpoint only accepts POST requests. See docs.")
 
+  # Find next available file
   path = os.path.join(BASE_DIR, "articles/")
   id = 0
   while os.path.exists(f"{path}{id}"): id += 1 
 
+  # Get data from request
   post_data = request.POST
   try:
     article = {
@@ -70,6 +105,8 @@ def post_article(request):
     }
   except KeyError:
     return HttpResponseBadRequest("Bad data format. See docs.")
+
+  # Data was okay
   else:
     with open(f"{path}{id}", "w") as f:
       json.dump(article, f)
