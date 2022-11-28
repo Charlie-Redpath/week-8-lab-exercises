68ac1c54c676b2d283f498d42b0aeda49d2e763d
Charlie Redpath <Charlie.Redpath@warwick.ac.uk>
2022-11-28 11:08:39 +0000

    Port API to python

--- API/urls.py
+++ API/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('article/<int:id>', views.get_article, name="article"),
+    path('article', views.post_article, name="post_article"),
 ]
