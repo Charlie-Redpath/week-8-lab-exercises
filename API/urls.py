f88ee8ed62fee6ebd8a167525130597095d09f6e
Charlie Redpath <Charlie.Redpath@warwick.ac.uk>
2022-11-28 11:36:06 +0000

    Remove admin

--- API/urls.py
+++ API/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('article/<int:id>', views.get_article, name="article"),
     path('article', views.post_article, name="post_article"),
 ]
