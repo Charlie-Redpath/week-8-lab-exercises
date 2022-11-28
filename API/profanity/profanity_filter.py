50bbb20a65399c57f5387ac037a91cd419427466
Charlie Redpath <charlie.redpath@warwick.ac.uk>
2022-11-28 23:29:31 +0000

    Finally sorted filter

---
+++ API/profanity/profanity_filter.py
@@ -0,0 +1,102 @@
+from .utils import (get_complete_path, read_wordList)
+from .trie import Trie
+
+
+class ProfanityFilter:
+    def __init__(self):
+        self.CHARS_MAPPING = {
+            "a": ("a", "@", "*", "4"),
+            "i": ("i", "*", "l", "1"),
+            "o": ("o", "*", "0", "@"),
+            "u": ("u", "*", "v"),
+            "v": ("v", "*", "u"),
+            "l": ("l", "1"),
+            "e": ("e", "*", "3"),
+            "s": ("s", "$", "5"),
+            "t": ("t", "7")
+        }
+        self.censor_urls = set()
+        self.profane_trie = Trie()
+        self.default_wordlist_filename = get_complete_path('API/profanity/data/profanity_wordlist.txt')
+        self.default_urls_filename = get_complete_path('API/profanity/data/profane_sites.txt')
+
+        self.load_profane_words(profane_words=None, whitelist_words=None)
+        self.load_profane_urls()
+
+    def load_profane_words(self, profane_words, whitelist_words):
+        self.profane_trie = Trie()
+        if profane_words is None:
+            profane_words = read_wordList(self.default_wordlist_filename)
+        self.generate_possible_profane_words(profane_words, whitelist_words)
+
+    def generate_possible_profane_words(self, profane_words, whitelist_words):
+        for profane_word in profane_words:
+            self.dfs(profane_word, 0, [], whitelist_words)
+
+    def dfs(self, profane_word, idx, char_list, whitelist_words):
+        if idx == len(profane_word):
+            possible_profane_word = ''
+            for char in char_list:
+                possible_profane_word += char
+            if whitelist_words is None or possible_profane_word not in whitelist_words:
+                self.profane_trie.insert(possible_profane_word)
+            return
+
+        if profane_word[idx] not in self.CHARS_MAPPING:
+            char_list.append(profane_word[idx])
+            self.dfs(profane_word, idx + 1, char_list, whitelist_words)
+            char_list.pop(len(char_list) - 1)
+
+        else:
+            for char in self.CHARS_MAPPING[profane_word[idx]]:
+                char_list.append(char)
+                self.dfs(profane_word, idx + 1, char_list, whitelist_words)
+                char_list.pop(len(char_list) - 1)
+
+    def load_profane_urls(self):
+        profane_urls = read_wordList(self.default_urls_filename)
+        for url in profane_urls:
+            self.censor_urls.add(url)
+
+    def censor_url(self, url):
+        if self.censor_urls.__contains__(url):
+            return '*'*len(url)
+        return url
+
+    def censor(self, text, censor_char="*"):
+
+        if type(text) != str:
+            text = str(text)
+        if type(censor_char) != str:
+            censor_char = str(censor_char)
+
+        if self.profane_trie.root is None:
+            self.load_profane_words()
+
+        return self.censor_profane_words(text, censor_char)
+
+    def censor_profane_words(self, message, censor_char):
+        message = message.split()
+        clean_message = ''
+        for word in message:
+            curr_word = ''
+            if self.profane_trie.hasPrefix(word.lower()):
+                for i in range(len(word)):
+                    curr_word += censor_char
+            else:
+                curr_word = word
+            clean_message += curr_word + ' '
+        return clean_message
+
+    def isProfane(self, word):
+        if self.profane_trie.hasPrefix(word):
+            return True
+        return False
+
+    def add_profane_words(self, words):
+        for word in words:
+            self.profane_trie.insert(word)
+
+    def add_whitelist_words(self, words):
+        for word in words:
+            self.whiteList_trie.insert(word)
