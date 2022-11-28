291bf6bae9da18faf4e8fd22413bec00fb90dd7b
Charlie Redpath <charlie.redpath@warwick.ac.uk>
2022-11-28 23:20:49 +0000

    Update setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,3 @@
 python -m venv venv
 source venv/bin/activate
 pip install django
-
-# Get "bad words"
-wget "https://raw.githubusercontent.com/arhankundu99/profanity-filter/master/data/profanity_wordlist.txt" -qO API/profanity.txt
